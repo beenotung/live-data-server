87a4a0013feb6533f3da2dd30c2d1f9585862411
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-08-07 22:39:03 +0800

    update deps; replace multer with formidable to reduce memory consumption and increase throughput;

--- src/file.ts
+++ src/file.ts
@@ -1,33 +1,43 @@
 import express, { Router } from 'express'
-import multer from 'multer'
+import formidable from 'formidable'
+import fs from 'fs'
 
 let routes = Router()
 export default routes
 
 let counter = 0
 
-let disk = multer.diskStorage({
-  destination: 'uploads',
-  filename: (req, file, cb) => {
-    counter++
-    let ext = file.mimetype.split('/').pop()
-    let time = Date.now()
-    let filename = `${time}-${counter}.${ext}`
-    cb(null, filename)
-  },
-})
+let uploadDir = 'uploads'
 
-let upload = multer({ storage: disk })
+fs.mkdirSync(uploadDir, { recursive: true })
 
-routes.post('/', upload.array('file'), (req, res) => {
-  if (Array.isArray(req.files)) {
-    let filenames = req.files.map(file => file.filename)
-    res.json({ filenames })
-  } else {
-    res
-      .status(400)
-      .json({ error: 'Invalid format, should have req.files array' })
-  }
+routes.post('/', (req, res, next) => {
+  let form = new formidable.Formidable({
+    uploadDir,
+    filename(name, ext, part, form): string {
+      counter++
+      let extname = part.mimetype?.split('/').pop()
+      let time = Date.now()
+      let filename = `${time}-${counter}.${extname}`
+      return filename
+    },
+  })
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err)
+      return
+    }
+    let file = files.file
+    if (Array.isArray(file)) {
+      let filenames = file.map(file => file.newFilename)
+      res.json({ filenames })
+    } else if (file) {
+      let filenames = [file.newFilename]
+      res.json({ filenames })
+    } else {
+      res.status(400).json({ error: 'Invalid format, missing file' })
+    }
+  })
 })
 
 routes.use('/', express.static('uploads'))
